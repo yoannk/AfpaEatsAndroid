7d7a79e995149e6d980d6b9b98a809a9dc49d6ed
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-23 10:52:45 +0200

    création TabbedActivity pour RestosCuisines

--- app/src/main/java/com/example/afpaeats/RestosCuisinesActivity.java
+++ app/src/main/java/com/example/afpaeats/RestosCuisinesActivity.java
@@ -1,4 +1,27 @@
 package com.example.afpaeats;
 
-public class RestosCuisinesActivity {
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.afpaeats.ui.main.SectionsPagerAdapter;
+
+public class RestosCuisinesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restos_cuisines);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
 }
