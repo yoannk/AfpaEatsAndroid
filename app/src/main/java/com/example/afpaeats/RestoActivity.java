585f15e952bb2e16a8aa1a9f914dd30e27083a28
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-27 11:38:28 +0200

    maj RestoActivity

--- app/src/main/java/com/example/afpaeats/RestoActivity.java
+++ app/src/main/java/com/example/afpaeats/RestoActivity.java
@@ -1,6 +1,9 @@
 package com.example.afpaeats;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,13 +12,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.afpaeats.Adapters.ProduitsAdapter;
+import com.example.afpaeats.Adapters.RestaurantsAdapter;
+import com.example.afpaeats.Entities.Produit;
+import com.example.afpaeats.Entities.Produits;
 import com.example.afpaeats.Entities.Restaurant;
+import com.example.afpaeats.Utilities.App;
 import com.example.afpaeats.Utilities.Constants;
+import com.example.afpaeats.Utilities.ServiceWebTask;
 import com.google.gson.Gson;
 
-public class RestoActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class RestoActivity extends AppCompatActivity implements ServiceWebTask.InterfaceServiceWeb {
 
     Context context;
+    RecyclerView rvwProduits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +49,47 @@ public class RestoActivity extends AppCompatActivity {
 
             txtNom.setText(restaurant.getNom());
 
+            rvwProduits = findViewById(R.id.rvwProduits);
 
+            HashMap<String, String> parameters = new HashMap<>();
+            parameters.put("idRestaurant", "" + restaurant.getIdRestaurant());
+            parameters.put("idSw", "0");
+
+            ServiceWebTask serviceWebTask = new ServiceWebTask("GetProduits", parameters, 0, RestoActivity.this);
+            serviceWebTask.execute();
 
         } else {
             Toast.makeText(context, "Erreur lors du chargement des informations de restaurant.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void returnSw(String value, int idSw) {
+        if (!value.isEmpty()) {
+            Gson gson = new Gson();
+
+            switch (idSw) {
+                case 0:
+                    Produits produits = gson.fromJson(value, Produits.class);
+
+                    //on instancie notre adapter
+                    ProduitsAdapter produitsAdapter = new ProduitsAdapter(produits, context);
+
+                    //la manière dont les adherents doivent s'afficher
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
+
+
+                    rvwProduits.setLayoutManager(layoutManager);
+
+                    //Effet sur le recyclerView
+                    DefaultItemAnimator defaultItemAnimator = new DefaultItemAnimator();
+                    rvwProduits.setItemAnimator(defaultItemAnimator);
+
+                    //on passe notre adapter à notre recyclerview
+                    rvwProduits.setAdapter(produitsAdapter);
+
+                    break;
+            }
         }
     }
 }
